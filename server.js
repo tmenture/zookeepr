e4ad75e604b9c304a93617816f61f7633e910877
tmenture <thomasoxemail@gmail.com>
2022-05-11 13:48:19 -0400

    added proper port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express(); 
 
 const { animals } = require('./data/animals');
@@ -55,5 +56,5 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.listen(3001, () => {
-    console.log(`API server now on port 30001!`);
+    console.log(`API server now on port ${PORT}!`);
 });
