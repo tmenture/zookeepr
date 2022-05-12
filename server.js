278d3955f402c7b1eb0a64c50fb695cc627206df
tmenture <thomasoxemail@gmail.com>
2022-05-12 19:31:51 -0400

    hopefully fixed middleware issue

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const PORT = process.env.PORT || 3001;
-const app = express(); 
 
-
-// Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-
-// Middleware that instructs the server to make certain files readily available
+app.use(express.json());
 app.use(express.static('public'));
 
-// Pare incoming JSON data
-app.use(express.json());
-
+// Use apiRoutes
 app.use('/api', apiRoutes);
-
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
